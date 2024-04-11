146a28359df63d933d137e55357cdd3a61589d25
Ferguson1052 <162065720+Ferguson1052@users.noreply.github.com>
2024-04-11 02:14:44 -0400

    Update Project_5463.m
    added motion ability

--- Project_5463.m
+++ Project_5463.m
@@ -5,7 +5,7 @@ close
 %% Initialize the Environment
 
 WORLD = collisionBox(4,2,0.1); % XYZ Lengths
-WORLD.Pose = trvec2tform([1 0 -.05]); % XYZ Position
+WORLD.Pose = trvec2tform([1 0 -.0525]); % XYZ Position
 Wall1 = collisionBox(0.5,0.25,0.65); % XYZ Lengths
 Wall1.Pose = trvec2tform([0.5 0.5 0.325]); % XYZ Position
 Wall2 = collisionBox(0.25,0.25,0.85); % XYZ Lengths
@@ -23,7 +23,7 @@ hold on
 
 manipulator = loadrobot("universalUR5e", DataFormat="row", Gravity=[0 0 -9.81]);
 showdetails(manipulator)
-q = homeConfiguration(manipulator);
+config = homeConfiguration(manipulator);
 
 
 %% Initialize Human Entity
@@ -35,7 +35,7 @@ figure("Name","Pick and Place Using RRT",...
     "Units","normalized",...
     "OuterPosition",[0, 0, 1, 1],...
     "Visible","on");
-show(manipulator,q,"Visuals","off","Collisions","on");
+show(manipulator,config,"Visuals","off","Collisions","on");
 hold on
 for i = 1:length(env)
     show(env{i})
@@ -44,3 +44,26 @@ view(60,15)
 xlabel('x')
 ylabel('y')
 zlabel('z')
+
+%% Path Planning
+planner = manipulatorRRT(manipulator, env);
+planner.SkippedSelfCollisions='parent';
+
+planner.MaxConnectionDistance = 0.3;
+planner.ValidationDistance = 0.1;
+
+startConfig = config;
+goalConfig = [-pi/4    -0.7*pi    pi/4   -pi/2   pi/2    -pi/2];
+rng('default');
+path = plan(planner,startConfig,goalConfig);
+
+interpStates = interpolate(planner, path);
+
+for i = 1:2:size(interpStates,1)
+    show(manipulator, interpStates(i,:),...
+        "PreservePlot", false,...
+        "Visuals","off",...
+        "Collisions","on");
+    title("Plan 1: MaxConnectionDistance = 0.3")
+    drawnow;
+end
